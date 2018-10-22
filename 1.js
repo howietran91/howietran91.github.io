f36fdd76f7200411238fdb405c0b7d7a7fc39ef7
howietran91 <howietran91@gmail.com>
2018-10-22 09:48:36 +0700

    them js cho cac tab san pham

--- 1.js
+++ 1.js
@@ -2,6 +2,11 @@ $(function(){
 	$('.tab ul li a').click(function() {
 		$('.tab ul li a').removeClass("active");
 		$(this).addClass("active");
+
+		$dem = $(this).parent().index()+1;
+		
+		$('.tabsanpham').removeClass("active");
+		$('.tabsanpham:nth-child(' + $dem +')').addClass("active");
 		return false;
 	})
 })
